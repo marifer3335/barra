e53db673f2d1fe97576c7ac6be2f862f5d06c5cf
Maria fernanda <marifer3335@hotmail.com>
2018-05-16 15:00:55 -0500

    avance 16

--- pos/src/app/barra-menu/barra-menu.component.ts
+++ pos/src/app/barra-menu/barra-menu.component.ts
@@ -4,12 +4,22 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-barra-menu',
   templateUrl: './barra-menu.component.html',
   styleUrls: ['./barra-menu.component.css']
+ 
 })
 export class BarraMenuComponent implements OnInit {
+  manzana="manzana"
+  mac="Mac"
+  ipad="iPad"
+  iphone="iPhone"
+  watch="Watch"
+  tv="Tv"
+  music="Music"
+  soporte="Soporte"
 
   constructor() { }
 
   ngOnInit() {
+   
   }
 
 }
